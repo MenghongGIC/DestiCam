f96535ce7090635222887adf40dc5a1c2743242b
unknown <istartgame31@gmail.com>
2026-03-27 09:21:31 +0700

    add tour_guide page

--- src/routes/tour_guide/index.tsx
+++ src/routes/tour_guide/index.tsx
@@ -3,26 +3,15 @@ import Counter from "~/components/Counter";
 
 import styles from "./index.module.css";
 
+import NavigationBar from "~/components/NavigationBar";
 
 export default function TourGuide() {
     return (
         <main>
             <Title>Hello World</Title>
 
-            <h1
-                class={styles.title}
-            >Gour Guide Page
-                <p
-                    class={styles.this_p}
-                >This is P</p>
-            </h1>
-            <p>
-                Visit{" "}
-                <a href="https://start.solidjs.com" target="_blank">
-                start.solidjs.com
-                </a>{" "}
-                to learn how to build SolidStart apps.
-            </p>
+            <NavigationBar />
+
         </main>
     );
 }
