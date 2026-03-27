2e1e9cb2c464670e66c458f948916ae94a32ca70
unknown <istartgame31@gmail.com>
2026-03-27 09:03:28 +0700

    update global context

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,45 @@
 import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
+import { Suspense, createContext, createSignal, onMount, onCleanup } from "solid-js";
+import type { Accessor } from "solid-js";
 import "./app.css";
 
+export const GlobalContext = createContext<{ 
+  innerHeight: Accessor<number>; 
+  innerWidth: Accessor<number>
+}>({
+  innerHeight: () => 0,
+  innerWidth: () => 0
+});
+
 export default function App() {
+  const [innerHeight, setInnerHeight] = createSignal(0);
+  const [innerWidth, setInnerWidth] = createSignal(0);
+
+  onMount(() => {
+    const handler = () => {
+      setInnerWidth(window.innerWidth);
+      setInnerHeight(window.innerHeight);
+    };
+    handler();
+
+    window.addEventListener("resize", handler);
+    onCleanup(() => window.removeEventListener("resize", handler));
+  });
+
   return (
-    <Router
-      root={props => (
-        <MetaProvider>
-          <Title>DestiCam</Title>
-          <Suspense>{props.children}</Suspense>
-        </MetaProvider>
-      )}
-    >
-      <FileRoutes />
-    </Router>
+    <GlobalContext.Provider value={{ innerHeight, innerWidth }}>
+      <Router
+        root={props => (
+          <MetaProvider>
+            <Title>DestiCam</Title>
+            <Suspense>{props.children}</Suspense>
+          </MetaProvider>
+        )}
+      >
+        <FileRoutes />
+      </Router>
+    </GlobalContext.Provider>
   );
 }
