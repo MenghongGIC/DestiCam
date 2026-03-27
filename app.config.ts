020323dc0b6c9d5cf1ff30f6d6fefcbfa4fb7a49
unknown <istartgame31@gmail.com>
2026-03-27 12:37:17 +0700

    add tailwind and daisyui

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
+
 import webfontDownload from 'vite-plugin-webfont-dl'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     vite: {
-        plugins: [webfontDownload()]
-    }
+        plugins: [tailwindcss(), webfontDownload()],
+    },
 });
