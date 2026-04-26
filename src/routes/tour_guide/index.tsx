2b2dc06fc175c75dc253de50bf1c07861efc88c8
GoodDay360 <istartgame31@gmail.com>
2026-04-27 01:42:17 +0700

    add proper filter + update dropdown

--- src/routes/tour_guide/index.tsx
+++ src/routes/tour_guide/index.tsx
@@ -1,5 +1,6 @@
 import { Title } from "@solidjs/meta";
-import { For, onMount, createSignal, onCleanup } from "solid-js";
+import { For, onMount, createSignal, onCleanup, on, createEffect } from "solid-js";
+import type { Setter, Accessor } from "solid-js";
 
 import styles from "./index.module.css";
 
@@ -19,6 +20,24 @@ export default function TourGuide() {
     const [innerWidth, setInnerWidth] = createSignal(0);
 
     const [is_show_more_filter, set_is_show_more_filter] = createSignal(false);
+
+    const [search, set_search] = createSignal<string>("");
+    const [price, set_price] = createSignal<{from: number, to: number}>({from:0, to:0});
+    const [language, set_language] = createSignal<string>("");
+
+    const [filtered_data, set_filtered_data] = createSignal<data_type>(data);
+    
+    
+    createEffect(on([search, price, language], () => {
+        console.log("A", language());
+        set_filtered_data(data.filter((item) => 
+            (search() !== "" ? item.name.toLowerCase().includes(search().toLowerCase()) : true)
+            && ((price().from < price().to) ? (((item.price_per_day >= price().from) && (item.price_per_day <= price().to))) : true)
+            && (language() !== "" ? item.languages_spoken.includes(language()) : true)
+        ));
+    
+    }));
+
     
     onMount(() => {
         const handler = () => {
@@ -30,12 +49,19 @@ export default function TourGuide() {
         onCleanup(() => window.removeEventListener("resize", handler));
     });
 
+
+
+
     return (
         <main class={styles.main}>
             <Title>DestiCam - Tour Guide</Title>
             <NavigationBar />
 
-            <FilterContainer1/>
+            <FilterContainer1
+                search={search}
+                set_search={set_search}
+                set_language={set_language}
+            />
             {innerWidth() < 800 && 
                 <div class={styles.show_more_btn_container}>
                     <button class={`btn btn-neutral ${styles.show_more_btn}`}
@@ -63,20 +89,26 @@ export default function TourGuide() {
             {(is_show_more_filter() && innerWidth() < 800) && 
                 <>
                     <div class="divider"/>
-                    <FilterContainer2/>
+                    <FilterContainer2
+                        price={price}
+                        set_price={set_price}
+                    />
                     <div class="divider"/>
                 </>
             }
 
             <div class={styles.body_container}>
                 {innerWidth() >= 800 && <>
-                    <FilterContainer2/>
+                    <FilterContainer2
+                        price={price}
+                        set_price={set_price}
+                    />
                     <div class="divider divider-horizontal"/>
                 </>}
 
                 
                 <div class={styles.item_container}>
-                    <For each={data}>
+                    <For each={filtered_data()}>
                         {(data_info) => (
                             <ItemComponent data_info={data_info}/>
                         )}
@@ -89,89 +121,122 @@ export default function TourGuide() {
     );
 }
 
-function FilterContainer1(){
+function FilterContainer1({
+    search, set_search,
+    set_language,
+
+}:{
+    search: Accessor<string>,
+    set_search: Setter<string>,
+    set_language: Setter<string>
+}){
 
     return (<>
         <div class={styles.filter_container_1}>
-            <Dropdown
-                label="Location"
-                label_class={styles.dropdown_label}
-                dropdown_class={styles.dropdown}
-                onChange={(e) => {
-                    console.log(e)
-                }}
-                data={[
-                    {
-                        id: 1,
-                        value: "Phnom Penh"
-                    },
-                    {
-                        id: 2,
-                        value: "Siem Reap"
-                    }
-                ]}
-            />
-
-            <Dropdown
-                label="Language"
-                label_class={styles.dropdown_label}
-                dropdown_class={styles.dropdown}
-                onChange={(e) => {
-                    console.log(e)
-                }}
-                data={[
-                    {
-                        id: 1,
-                        value: "English"
-                    },
-                    {
-                        id: 2,
-                        value: "French"
-                    },
-                    {
-                        id: 3,
-                        value: "Khmer"
-                    },
-                ]}
-            />
-            <Datepicker
-                label="Available Date"
-            />
-
-            <Dropdown
-                label="Number of Travellers"
-                label_class={styles.dropdown_label}
-                dropdown_class={styles.dropdown}
-                onChange={(e) => {
-                    console.log(e)
-                }}
-                data={[
-                    {
-                        id: 1,
-                        value: "1"
-                    },
-                    {
-                        id: 2,
-                        value: "2"
-                    },
-                    {
-                        id: 3,
-                        value: "3"
-                    },
-                    {
-                        id: 4,
-                        value: "4"
-                    },
-                    {
-                        id: 5,
-                        value: "4+"
-                    },
-                ]}
-            />
+            <div class={styles.box}>
+                <Dropdown
+                    label="Location"
+                    label_class={styles.dropdown_label}
+                    dropdown_class={styles.dropdown}
+                    
+                    data={[
+                        {
+                            id: 1,
+                            value: "Phnom Penh"
+                        },
+                        {
+                            id: 2,
+                            value: "Siem Reap"
+                        }
+                    ]}
+                />
             
-            <button 
-                class={`btn btn-neutral ${styles.search_btn}`}
-            >Search</button>
+
+                <Dropdown
+                    label="Language"
+                    label_class={styles.dropdown_label}
+                    dropdown_class={styles.dropdown}
+                    onChange={(e) => {
+                        console.log("p", e.value?.toString() ?? "");
+                        set_language(e.value?.toString() ?? "");
+                    }}
+                    data={[
+                        {
+                            id: 1,
+                            value: "English"
+                        },
+                        {
+                            id: 2,
+                            value: "French"
+                        },
+                        {
+                            id: 3,
+                            value: "Khmer"
+                        },
+                    ]}
+                />
+                <Datepicker
+                    label="Available Date"
+                />
+
+                <Dropdown
+                    label="Number of Travellers"
+                    label_class={styles.dropdown_label}
+                    dropdown_class={styles.dropdown}
+                    onChange={(e) => {
+                        console.log(e)
+                    }}
+                    data={[
+                        {
+                            id: 1,
+                            value: "1"
+                        },
+                        {
+                            id: 2,
+                            value: "2"
+                        },
+                        {
+                            id: 3,
+                            value: "3"
+                        },
+                        {
+                            id: 4,
+                            value: "4"
+                        },
+                        {
+                            id: 5,
+                            value: "4+"
+                        },
+                    ]}
+                />
+            </div>
+            <div class={styles.box}>
+                <label class={`input ${styles.search_input}`}>
+                    <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <g
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                        stroke-width="2.5"
+                        fill="none"
+                        stroke="currentColor"
+                        >
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                        </g>
+                    </svg>
+                    <input type="search" required placeholder="Search" 
+                        value={search()}
+                        onInput={(e: any) => {
+                            set_search(e.target.value)
+                            console.log("hi");
+                        }}
+                    />
+                </label>
+                <button 
+                    class={`btn btn-neutral ${styles.search_btn}`}
+                >Search</button>
+            </div>
+            
         
             
         </div>
@@ -180,7 +245,14 @@ function FilterContainer1(){
 
 
 
-function FilterContainer2(){
+function FilterContainer2({
+    price, set_price,
+
+}:{
+    price: Accessor<{from: number, to: number}>,
+    set_price: Setter<{from: number, to: number}>
+    
+}){
 
     return (<>
         <div class={styles.filter_container_2}>
@@ -189,17 +261,30 @@ function FilterContainer2(){
                 <div class={styles.item_box}>
                     <div class={styles.input_box}>
                         <label class={styles.input_label} for="price_from">From: </label>
-                        <input  name="price_from" type="text" placeholder="$0" class={`input input-neutral ${styles.input}`} />
+                        <input  name="price_from" type="text" placeholder="$0" class={`input input-neutral ${styles.input}`} 
+                            value={price().from}
+                            onInput={(e: any) => {
+                                const value = parseFloat(e.target.value);
+                                console.log(value);
+                                set_price({...price(), from: value})
+                            }}
+                        />
                     </div>
 
                     <div class={styles.input_box}>
                         <label class={styles.input_label} for="price_to">To: </label>
-                        <input name="price_to" type="text" placeholder="$1000" class={`input input-neutral ${styles.input}`} />
+                        <input name="price_to" type="text" placeholder="$0" class={`input input-neutral ${styles.input}`} 
+                            value={price().to}
+                            onInput={(e: any) => {
+                                const value = parseFloat(e.target.value);
+                                console.log(value);
+                                set_price({...price(), to: value})
+                            }}
+                        />
                     </div>
                 </div>
             </div>
             <div class="divider"/>
-
             
             <div class={styles.box}>
                 <span class={styles.section_label}>Years of Experience</span>
