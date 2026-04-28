f21eb71fa504998c16343e73141347d7afead816
GoodDay360 <istartgame31@gmail.com>
2026-04-28 17:27:10 +0700

    fix: clean up unaccept changes

--- src/routes/things_to_do/index.tsx
+++ src/routes/things_to_do/index.tsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD:src/routes/ThingToDo/index.tsx
-import { Title } from "@solidjs/meta";
-
-import styles from "./index.module.css";
-import NavigationBar from "~/components/NavigationBar";
-
-export default function ThingsToDo() {
-    return (
-        <main class={styles.main}>
-            <Title>DestiCam - Things To Do</Title>
-            <NavigationBar />
-
-            <div class={styles.container}>
-                <h1 class={styles.title}>Things To Do</h1>
-                <p class={styles.description}>
-                    Discover the best attractions and activities for your next adventure.
-                </p>
-                <div class={styles.placeholder_grid}>
-                    <p>Coming Soon...</p>
-                </div>
-            </div>
-        </main>
-    );
-}
-=======
 import { Title } from "@solidjs/meta";
 import Counter from "~/components/Counter";
 
@@ -51,4 +26,3 @@ export default function TourGuide() {
         </main>
     );
 }
->>>>>>> origin/main:src/routes/things_to_do/index.tsx
